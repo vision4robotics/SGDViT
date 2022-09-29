63a951b03a42a5768450d515a60ca3f1600f7c31
reverie <65004627+Reverie5@users.noreply.github.com>
2022-09-29 13:22:37 +0800

    Add files via upload

--- pysot/models/loss.py
+++ pysot/models/loss.py
@@ -29,18 +29,7 @@ def select_cross_entropy_loss(pred, label):
     loss_neg = get_cls_loss(pred, label, neg)
     return loss_pos * 0.5 + loss_neg * 0.5
 
-def l1loss(pre,label,weight):
-    loss=(torch.abs((pre-label))*weight).sum()/(weight).sum()
-    return loss
-
-def weight_l1_loss(pred_loc, label_loc, loss_weight):
-    b, _, sh, sw = pred_loc.size()
-    #pred_loc = pred_loc.view(b, 4, -1, sh, sw)
-    diff = (pred_loc - label_loc).abs()
-    diff = diff.sum(dim=1).view(b, -1, sh, sw)
-    loss = diff * loss_weight
-    return loss.sum().div(b)/(loss_weight).sum()
-
+            
 class IOULoss(nn.Module):
     def forward(self, pred, target, weight=None):
         
@@ -67,61 +56,10 @@ class IOULoss(nn.Module):
         area_union = target_aera + pred_aera - area_intersect
         ious=((area_intersect ) / (area_union +1e-6)).clamp(min=0)+1e-6
         
-        losses = -torch.log(ious)
+        losses = 1-ious
         weight=weight.view(losses.size())
         if weight.sum()>0:
             
             return (losses * weight).sum() / (weight.sum()+1e-6)
         else:
             return (losses *weight).sum()
-            
-class gIOULoss(nn.Module):
-    def forward(self, pred, target, weight=None):
-        
-        pred_left = pred[:,:, 0]
-        pred_top = pred[:,:, 1]
-        pred_right = pred[:,:, 2]
-        pred_bottom = pred[:,:, 3]
-
-        target_left = target[:,:, 0]
-        target_top = target[:,:, 1]
-        target_right = target[:,:, 2]
-        target_bottom = target[:,:, 3]
-        
-        x1 = torch.min(pred_left, pred_right)
-        y1 = torch.min(pred_top, pred_bottom)
-        x2 = torch.max(pred_left, pred_right)
-        y2 = torch.max(pred_top, pred_bottom)
-    
-        xkis1 = torch.max(x1, target_left)
-        ykis1 = torch.max(y1, target_top)
-        xkis2 = torch.min(x2, target_right)
-        ykis2 = torch.min(y2, target_bottom)
-    
-        xc1 = torch.min(x1, target_left)
-        yc1 = torch.min(y1, target_top)
-        xc2 = torch.max(x2, target_right)
-        yc2 = torch.max(y2, target_bottom)
-    
-        intsctk = torch.zeros(x1.size()).cuda()
-        
-        mask = (ykis2 > ykis1) * (xkis2 > xkis1)
-        intsctk[mask] = (xkis2[mask] - xkis1[mask]) * (ykis2[mask] - ykis1[mask])
-        unionk = (x2 - x1) * (y2 - y1) + (target_right - target_left) * (target_bottom - target_top) - intsctk + 1e-7
-        iouk = intsctk / unionk
-    
-        area_c = (xc2 - xc1) * (yc2 - yc1) + 1e-7
-        miouk = iouk - ((area_c - unionk) / area_c)
-        
-        # iouk = ((1 - iouk) * iou_weights).sum(0) / batch_size
-        # miouk = ((1 - miouk) * iou_weights).sum(0) / batch_size
-
-
-        losses = 1-miouk
-        weight=weight.view(losses.size())
-        if weight.sum()>0:
-            
-            return (losses * weight).sum() / (weight.sum()+1e-6)
-        else:
-            return (losses *weight).sum()
-            
