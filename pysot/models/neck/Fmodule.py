e4fb49e61b02a939284b45efd8340c97889ed30f
reverie <65004627+Reverie5@users.noreply.github.com>
2023-03-22 10:55:34 +0800

    Add files via upload
    adjust the name

--- pysot/models/neck/Fmodule.py
+++ pysot/models/neck/Fmodule.py
@@ -24,23 +24,8 @@ class Fmodule(nn.Module):
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             )
-        self.layer4= nn.Sequential(
-            nn.Conv2d(256, 256, kernel_size=3),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            )
-        self.layer5= nn.Sequential(
-            nn.Conv2d(256, 256, kernel_size=3),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            )
-        self.layer6= nn.Sequential(
-            nn.ConvTranspose2d(256, 256, kernel_size=3),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            )
         self.layer7= nn.Sequential(
-            nn.Conv2d(dim, 256, kernel_size=4),
+            nn.Conv2d(256, 256, kernel_size=3),
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             )
@@ -49,26 +34,41 @@ class Fmodule(nn.Module):
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             )
-        self.layer9= nn.Sequential(
-            nn.Conv2d(256, 256, kernel_size=3),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            )
-        self.layer10= nn.Sequential(
+        self.layer4= nn.Sequential(
             nn.ConvTranspose2d(256, 256, kernel_size=3),
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             )
-        self.layer11= nn.Sequential(
+        self.layer5= nn.Sequential(
             nn.Conv2d(256, 1, kernel_size=1,stride=1),
             nn.BatchNorm2d(1),
             nn.ReLU(inplace=True),
             )
-        self.layer12= nn.Sequential(
+        self.layer6= nn.Sequential(
             nn.Conv2d(256, 256, kernel_size=1,stride=1),
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
         )
+        self.layer9= nn.Sequential(
+            nn.Conv2d(dim, 256, kernel_size=4),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            )
+        self.layer10= nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            )
+        self.layer11= nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            )
+        self.layer12= nn.Sequential(
+            nn.ConvTranspose2d(256, 256, kernel_size=3),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            )
         self.layer= nn.Conv2d(dim,dim,kernel_size=2)
 
         self.mlp= Mlp(in_features=dim, out_features=dim,hidden_features=int(dim * mlp_ratio))
@@ -84,41 +84,41 @@ class Fmodule(nn.Module):
         out = out.view(batch, channel, out.size(2), out.size(3))
         return out
     
-    def Conv_1(self,x):
+    def cforward(self,x):
         x=self.layer1(x)
         x=self.layer2(x)
         return x
 
-    def Conv_2(self,x):
+    def CDConv(self,x):
         x=self.layer3(x)
-        x=self.layer4(x)
-        x=self.layer5(x)
-        x=self.layer6(x)
-        return x
-    
-    def Conv_3(self,x):
         x=self.layer7(x)
         x=self.layer8(x)
+        x=self.layer4(x)
+        return x
+    
+    def FASNet(self,x):
         x=self.layer9(x)
         x=self.layer10(x)
+        x=self.layer11(x)
+        x=self.layer12(x)
         return x
 
     def forward(self, x,z):
-        x=self.Conv_1(x)
-        z=self.Conv_1(z)
+        x=self.cforward(x)
+        z=self.cforward(z)
         saliency = self.xcorr_depthwise(x, z).permute(2,3,0,1)
       
         s=self.mlp(saliency)
         s=s.permute(2,3,0,1)
-        s=self.Conv_2(s)
+        s=self.CDConv(s)
 
         #the output of saliency features and saliency map
-        mask=self.layer11(s)
-        s=self.layer12(s)
+        mask=self.layer5(s)
+        s=self.layer6(s)
 
         #process the X
         x=self.layer(x)
-        x=self.Conv_3(x)
+        x=self.FASNet(x)
 
         return s,x,mask,saliency
 
